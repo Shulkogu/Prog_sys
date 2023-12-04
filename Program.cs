db52305928d0f586bc10dbe260033fc84acb1d4a
leo-jolibois <116551155+leo-jolibois@users.noreply.github.com>
2023-12-04 23:34:37 +0100

    Added Texts class and program entry point
    Added the class that has the program's CLI texts in French and English.
    Added the Program.cs file that initializes the View and that can automatically execute jobs if a -e argument was passed when executing the program.

--- Program.cs
+++ Program.cs
@@ -1,16 +1,32 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 using Model;
+using View;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Logger Logger = new Logger(@"C:\\PathToLogFile.json", @"C:\\PathToStateFile.json");
-        Logger.FinalizeLogs();
-        Saver saver = new Saver("Test", @"C:\\SourcePath", @"C:\\TargetPath", Savetype.Differential, ref Logger);
-        saver.SaveFiles();
-        Logger.FinalizeLogs();
+        //A jobview is created along with its list of existing jobs
+        List<Job> existingJobs = JobSaver.LoadExistingJobs(Constants.JobsFile);
+        View.JobView jobView = new View.JobView(ref existingJobs);
+        //If two arguments were passed when starting the .EXE, the first one being "-e", the second argument is sent as range to be used by the orchestrator to start jobs
+        if (args.Length == 2 && args[0] == "-e")
+        {
+            jobView.CallOrchestrator(args[1]);
+        }
+        //If no "-e" argument was passed when starting the .EXE, the CLI mode starts
+        else
+        {
+            jobView.EnterLanguageFromConsole();
+            //Until the user closes the program, the home page is displayed after he finishes interacting with one of the modes. The list of existing jobs is refreshed each time
+            while (jobView.UserInteraction())
+            {
+                existingJobs = JobSaver.LoadExistingJobs(Constants.JobsFile);
+                jobView.UpdateJobsList(existingJobs);
+            }
+        }
     }
 }
