35a267d8936224f126eea73915ddd675ce679265
leo-jolibois <116551155+leo-jolibois@users.noreply.github.com>
2023-12-10 10:06:11 +0100

    Applied the changes that were applied to version 1.1
    - To comply with the requirements for version 1.1, added XML logging option.
    - Added a settings file + a settings tab for the user to assign them
    - Removed trimming (unfortunately, when publishing this version with trimming, the rendered EXE file cannot serialize the settings file, so trimming has, for now, been disabled)

--- EasySave/Model/TypesAndConstants.cs
+++ EasySave/Model/TypesAndConstants.cs
@@ -1,8 +1,11 @@
-﻿﻿using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Model
@@ -25,9 +28,76 @@ namespace Model
         public static readonly string DateTimeFormat = @"dd-MM-yyyy HH:mm:ss";
         public static readonly string ExeLocation = AppDomain.CurrentDomain.BaseDirectory;
         public static readonly string JobsFile = ExeLocation+@"\\jobs.json";
+        public static readonly string SettingsFile = ExeLocation + @"\\settings.json";
         public static readonly string LogPath = ExeLocation+@"\\logs\\files\\";
         public static readonly string StatePath = ExeLocation+@"\\logs\\state\\";
         public static readonly string SingleToDoubleBackslashRegex = @"(?<!\\)\\{1}(?!\\)|\\{3,}";
+        public static readonly Settings Settings = new Settings();
+        public static string GetLoggerHeader()
+        {
+            switch (Constants.Settings.LogFileType.Value)
+            {
+                case LogFileType.XML:
+                    return ("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n<root>");
+                default:
+                    return("[");
+            }
+        }
+        public static string GetLoggerFooter()
+        {
+            switch (Constants.Settings.LogFileType.Value)
+            {
+                case LogFileType.XML:
+                    return ("\n</root>");
+                default:
+                    return ("\n]");
+            }
+        }
+    }
+    public class MutableEnum<TEnum>
+    {
+        //Class that encapsulates the Settings' Enums to make them mutable.
+        //This way, if any other Enum is added, the view's "adjust settings" tab automatically proposes them to the user without needing to modify its code
+        public TEnum Value { get; set; }
+
+        public MutableEnum(TEnum value)
+        {
+            Value = value;
+        }
+    }
+    public class Settings
+    {
+        //Singleton class that contains the program's settings
+        private static Lazy<Settings> _cache = new(() => new());
+        public static Settings Instance => _cache.Value;
+        [JsonInclude]
+        public MutableEnum<View.Language> Language = new MutableEnum <View.Language>(View.Language.English);
+        [JsonInclude]
+        public MutableEnum<LogFileType> LogFileType = new MutableEnum <LogFileType>(Model.LogFileType.JSON);
+        public bool LoadSettings()
+        //Function used to load the settings stored in the settings file.
+        //Returns true if a file existed and was loaded, otherwise, false.
+        {
+            if (System.IO.File.Exists(Constants.SettingsFile))
+            //If the settings file exists, it gets loaded into the object
+            {
+                try
+                {
+                    Settings Loaded = JsonSerializer.Deserialize<Settings>(System.IO.File.ReadAllText(Constants.SettingsFile))!;
+                    this.Language=Loaded.Language;this.LogFileType=Loaded.LogFileType;
+                    return true;
+                }
+                catch
+                {
+                }
+            }
+            return false;
+        }
+        public void SaveSettings()
+        {
+            string json = System.Text.Json.JsonSerializer.Serialize(this, new System.Text.Json.JsonSerializerOptions { WriteIndented = true });
+            System.IO.File.WriteAllText(Constants.SettingsFile, json);
+        }
     }
     public struct Existingsave
     {
@@ -60,14 +130,32 @@ namespace Model
         }
         public override string ToString()
         {
-            return $"\"Name\": \"{Name}\",\n" +
-                $"\"SourceFilePath\": \"{SourceFilePath}\",\n" +
-                $"\"TargetFilePath\": \"{TargetFilePath}\",\n" +
-                $"\"State\": \"{State}\",\n" +
-                $"\"TotalFilesToCopy\": \"{TotalFilesToCopy}\",\n" +
-                $"\"TotalFilesSize\": \"{TotalFilesSize}\",\n" +
-                $"\"NbFilesLeftToDo\": \"{TotalFilesToCopy - CurrentFile}\",\n" +
-                $"\"Progression\": \"{CurrentFile / TotalFilesToCopy * 100}%\"";
+            if(Constants.Settings.LogFileType.Value == LogFileType.XML)
+            {
+                return $"\n\t<row>\n" +
+                $"\t\t<Name>{Name}</Name>\n" +
+                $"\t\t<SourceFilePath>{SourceFilePath}</SourceFilePath>\n" +
+                $"\t\t<TargetFilePath>{TargetFilePath}</TargetFilePath>\n" +
+                $"\t\t<State>{State}</State>\n" +
+                $"\t\t<TotalFilesToCopy>{TotalFilesToCopy}</TotalFilesToCopy>\n" +
+                $"\t\t<TotalFilesSize>{TotalFilesSize}</TotalFilesSize>\n" +
+                $"\t\t<NbFilesLeftToDo>{TotalFilesToCopy - CurrentFile}</NbFilesLeftToDo>\n" +
+                $"\t\t<Progression>{CurrentFile / TotalFilesToCopy * 100}%</Progression>\n" +
+                $"\t</row>";
+            }
+            else
+            {
+                return $"\n{{\n" +
+                $"\t\"Name\": \"{Name}\",\n" +
+                $"\t\"SourceFilePath\": \"{SourceFilePath}\",\n" +
+                $"\t\"TargetFilePath\": \"{TargetFilePath}\",\n" +
+                $"\t\"State\": \"{State}\",\n" +
+                $"\t\"TotalFilesToCopy\": \"{TotalFilesToCopy}\",\n" +
+                $"\t\"TotalFilesSize\": \"{TotalFilesSize}\",\n" +
+                $"\t\"NbFilesLeftToDo\": \"{TotalFilesToCopy - CurrentFile}\",\n" +
+                $"\t\"Progression\": \"{CurrentFile / TotalFilesToCopy * 100}%\"\n" +
+                $"}},";
+            }
         }
     }
     public enum Savetype
@@ -79,5 +167,10 @@ namespace Model
     {
         ACTIVE,
         END
+    }
+    public enum LogFileType
+    {
+        JSON,
+        XML
     }
 }
