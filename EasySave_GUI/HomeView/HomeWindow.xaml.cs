5b29a8a390927042c961ca7c0e4c433548e6faf7
Shulkogu <gabriel.anciaux@gmail.com>
2023-12-19 13:52:45 +0100

    change images' path absolute to dynamic

--- EasySave_GUI/HomeView/HomeWindow.xaml.cs
+++ EasySave_GUI/HomeView/HomeWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -20,5 +21,24 @@ public partial class HomeWindow : Window
     public HomeWindow()
     {
         InitializeComponent();
+        
+        var FrenchImage = new BitmapImage(new Uri("pack://application:,,,/EasySave_GUI;component/icon/france.png"));
+        Resources["MyDynamicImage"] = FrenchImage;
+        var EnglishImage = new BitmapImage(new Uri("pack://application:,,,/EasySave_GUI;component/icon/royaume-uni.png"));
+        Resources["MyDynamicImage"] = EnglishImage;
+        var IconIcon = new BitmapImage(new Uri("pack://application:,,,/EasySave_GUI;component/icon/36020.ico"));
+        Resources["MyDynamicImage"] = IconIcon;
+        var HomeImage = new BitmapImage(new Uri("pack://application:,,,/EasySave_GUI;component/icon/accueil.png"));
+        Resources["MyDynamicImage"] = HomeImage;
+        var SaveImage = new BitmapImage(new Uri("pack://application:,,,/EasySave_GUI;component/icon/disquette.png"));
+        Resources["MyDynamicImage"] = SaveImage;
+        var LogsImage = new BitmapImage(new Uri("pack://application:,,,/EasySave_GUI;component/icon/un-journal.png"));
+        Resources["MyDynamicImage"] = LogsImage;
+        var SettingsImage = new BitmapImage(new Uri("pack://application:,,,/EasySave_GUI;component/icon/parametres.png"));
+        Resources["MyDynamicImage"] = SettingsImage;
+        var CreateImage = new BitmapImage(new Uri("pack://application:,,,/EasySave_GUI;component/icon/creer.png"));
+        Resources["MyDynamicImage"] = CreateImage;
+        var ModifyImage = new BitmapImage(new Uri("pack://application:,,,/EasySave_GUI;component/icon/bouton-modifier.png"));
+        Resources["MyDynamicImage"] = ModifyImage;
     }
 }
