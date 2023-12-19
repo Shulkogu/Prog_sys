32c18bdf97159e5651e925600b702914e75680a6
leo-jolibois <116551155+leo-jolibois@users.noreply.github.com>
2023-12-19 09:53:40 +0100

    Fixed parameters for CryptoSoft

--- EasySave/Model/File.cs
+++ EasySave/Model/File.cs
@@ -83,7 +83,7 @@ namespace Model
                     { 
                         Process CryptoSoftProcess = new Process();
                         CryptoSoftProcess.StartInfo.FileName = Constants.CryptoSoft;
-                        CryptoSoftProcess.StartInfo.Arguments = $"{((NewestVersion != null) ? $"-d {NewestVersion}" : "")}\"{this.Job.SourcePath + @"\\" + RelativeFilePath}\" \"{TargetFilePath}\" \"SUPERSECRETKEY\"";
+                        CryptoSoftProcess.StartInfo.Arguments = $"{((NewestVersion != null) ? $"-d \"{NewestVersion}\" " : "")}\"{this.Job.SourcePath + @"\\" + RelativeFilePath}\" \"{TargetFilePath}\" \"SUPERSECRETKEY\"";
                         Time = DateTimeOffset.Now.ToUnixTimeMilliseconds();
                         CryptoSoftProcess.Start();
                         CryptoSoftProcess.WaitForExit();
