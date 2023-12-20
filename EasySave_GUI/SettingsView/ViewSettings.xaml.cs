054ca105333927ea4ebea3776e558d42f2214200
leo-jolibois <116551155+leo-jolibois@users.noreply.github.com>
2023-12-20 11:29:33 +0100

    Added SettingsViewModel and linked it to View and Model

--- EasySave_GUI/SettingsView/ViewSettings.xaml.cs
+++ EasySave_GUI/SettingsView/ViewSettings.xaml.cs
@@ -4,71 +4,19 @@ using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using EasySave_GUI.SettingsViewModel;
 
 namespace EasySave_GUI.SettingsView;
 
     public partial class ViewSettings : UserControl
     {
-        public ObservableCollection<string> AddedExtensions { get; set; }
-        public ICommand AddExtensionCommand { get; set; }
-        public ICommand RemoveExtensionCommand { get; set; }
-        public string NewExtension { get; set; }
-
-        // Partie pour la fonction prioritaire 
-        public ObservableCollection<string> AddedPrioritizedExtensions { get; set; }
-        public ICommand AddPrioritizedExtensionCommand { get; set; }
-        public ICommand RemovePrioritizedExtensionCommand { get; set; }
-        public string NewPrioritizedExtension { get; set; }
-
-        // Partie choix langue 
-
         public string SelectedItem { get; set; }
         public ViewSettings()
         {
             InitializeComponent();
-
-
-            AddedExtensions = new ObservableCollection<string>();
-            AddExtensionCommand = new RelayCommand(AddExtension);
-            RemoveExtensionCommand = new RelayCommand<string>(RemoveExtension);
-
-            // Partie pour la fonction prioritaire 
-
-            AddedPrioritizedExtensions = new ObservableCollection<string>();
-            AddPrioritizedExtensionCommand = new RelayCommand(AddPrioritizedExtension);
-            RemovePrioritizedExtensionCommand = new RelayCommand<string>(RemovePrioritizedExtension);
-
-
-            DataContext = this;
-        }
-        private void AddExtension()
-        {
-            if (!string.IsNullOrEmpty(NewExtension))
-            {
-                if (Regex.IsMatch(NewExtension, @"^\..+$"))
-                {
-                    if (!AddedExtensions.Contains(NewExtension))
-                    {
-                        AddedExtensions.Add(NewExtension);
-                    }
-                    else
-                    {
-                        MessageBox.Show("Extension already exists in the list.");
-                    }
-
-                    NewExtension = string.Empty;
-                }
-                else
-                {
-                    MessageBox.Show("Invalid extension format. It should start with a dot (.)");
-                }
-            }
+            DataContext = new EasySave_GUI.SettingsViewModel.SettingsViewModel();
         }
 
-        private void RemoveExtension(string extension)
-        {
-            AddedExtensions.Remove(extension);
-        }
         private void ListBox_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
         {
             e.Handled = true;
@@ -79,7 +27,6 @@ namespace EasySave_GUI.SettingsView;
                 Source = listBox
             });
         }
-
 
         public class RelayCommand : ICommand
         {
@@ -125,40 +72,12 @@ namespace EasySave_GUI.SettingsView;
             public event EventHandler CanExecuteChanged;
         }
 
-        private void AddPrioritizedExtension()
-        {
-            if (!string.IsNullOrEmpty(NewPrioritizedExtension))
-            {
-                if (Regex.IsMatch(NewPrioritizedExtension, @"^\..+$"))
-                {
-                    if (!AddedPrioritizedExtensions.Contains(NewPrioritizedExtension))
-                    {
-                        AddedPrioritizedExtensions.Add(NewPrioritizedExtension);
-                    }
-                    else
-                    {
-                        MessageBox.Show("Extension already exists in the prioritized list.");
-                    }
-
-                    NewPrioritizedExtension = string.Empty;
-                }
-                else
-                {
-                    MessageBox.Show("Invalid extension format. It should start with a dot (.)");
-                }
-            }
-        }
-
-        private void RemovePrioritizedExtension(string extension)
-        {
-            AddedPrioritizedExtensions.Remove(extension);
-        }
-
         private void TextBox_GotFocus(object sender, RoutedEventArgs e)
         {
             TextBox textBox = sender as TextBox;
             if (textBox != null && textBox.Opacity == 0.5)
             {
+                textBox.Tag = textBox.Text;
                 textBox.Text = string.Empty;
                 textBox.Opacity = 1;
             }
@@ -169,9 +88,8 @@ namespace EasySave_GUI.SettingsView;
             TextBox textBox = sender as TextBox;
             if (textBox != null && string.IsNullOrWhiteSpace(textBox.Text))
             {
-                textBox.Text = "Placeholder Text";
+                textBox.Text = textBox.Tag.ToString();
                 textBox.Opacity = 0.5;
             }
         }
-
     }
